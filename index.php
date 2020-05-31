3a555d37aa05862903f33de3bfa20fdf05709b60
azquin <66213969+azquin@users.noreply.github.com>
2020-05-31 19:13:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include "_partials/header.php" ?>
+
+<div class="page-header">
+    <h1>nieco dasds</h1>
+</div>
+
+<ul class="list-group col-sm-6">
+    <li class="list-group-item"> yu very much need to do this</li>
+    <li class="list-group-item">but also this other thing</li>
+
+</ul>
+
+<form class="col-sm-6" action="_inc/add-new.php" method="post">
+        <p class="form-group">
+            <textarea class="form-control" name="message" id="text" placeholder="is ther smthing?" rows="3"></textarea>
+        </p>
+
+        <p class="form-group"><input type="submit" class="btn-sm btn-danger" value="add new thing">
+</form>
+
+
+
+<?php include "_partials/footer.php" ?>
